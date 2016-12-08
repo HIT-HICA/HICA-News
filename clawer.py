6dfc169914064ceea0305b43dc4519b4dfd9de20
i0x0i <ivan.i0x0i@gmail.com>
2016-12-08 14:26:50 +0800

    Fix bugs in exit() and articles naming

--- clawer.py
+++ clawer.py
@@ -1,5 +1,5 @@
-import os
 import re
+import sys
 import json
 import time
 import requests
@@ -10,15 +10,15 @@ headers = {'Accept-Language': 'en-US,en;q=0.5', 'Accept-Encoding': 'gzip, deflat
 res = requests.request(url=url,method=method,headers=headers)
 
 link = re.findall("(?<=main_toweixin_account_name_0.{8}).*?(?=\">)",res.text)
-link = os._exit(-1) if link==[] else link[0].replace('amp;','')
+link = sys.exit(-1) if link==[] else link[0].replace('amp;','')
 updateTime = re.findall("(?<=timeConvert.{2}).*?(?='\))",res.text)
-updateTime = os._exit(-2) if updateTime==[] else time.strftime("%Y-%m-%d", time.localtime(int(updateTime[0])))
+updateTime = sys.exit(-2) if updateTime==[] else time.strftime("%Y-%m-%d", time.localtime(int(updateTime[0])))
 
 #do we need to update?
 with open('index/index.json', 'r') as f:
 	oldList = json.load(f)
 if oldList['msgList'][-1]['id'][:-2] == updateTime:
-	os._exit(-3)
+	sys.exit(-3)
 
 #using the same  method
 url = link
@@ -26,7 +26,7 @@ headers = {'Accept-Language': 'en-US,en;q=0.5', 'Accept-Encoding': 'gzip, deflat
 res = requests.request(url=url,method=method,headers=headers)
 
 msg = re.findall("(?<=msgList.{3}).*(?=.{2})",res.text)
-msg = os._exit(-4) if msg==[] else json.loads(msg[0])
+msg = sys.exit(-4) if msg==[] else json.loads(msg[0])
 
 getUrl = lambda msgInfo: "http://mp.weixin.qq.com"+msgInfo['content_url'].replace('amp;','')
 getMsg = lambda msgInfo,id: {'author':msgInfo['author'],'cover':msgInfo['cover'],'digest':msgInfo['digest'],'id':id, 'title':msgInfo['title'],'source_url':msgInfo['source_url']}
@@ -40,6 +40,18 @@ if rawMsgs['is_multi'] == 1:
 	multiList = rawMsgs['multi_app_msg_item_list']
 	urlList += [getUrl(item) for item in multiList]
 	msgList += [getMsg(multiList[i],date+'-'+str(i+1)) for i in xrange(len(multiList))]
+	
+for i in xrange(len(urlList)):
+	#using the same method
+	url = urlList[i]
+	#using the same  headers
+	res = requests.request(url=url,method=method,headers=headers)
+	
+	content = re.findall('<section.*',res.text)
+	content = sys.exit(-5) if content==[] else content[0].replace('data-src','src')
+	with open('articles/'+str(msgList[i]['id']),'w') as f:
+		f.write(content.encode('utf-8'))
+	time.sleep(30)
 
 msgList = oldList['msgList'] + msgList
 if len(msgList) > 50:
@@ -48,15 +60,3 @@ if len(msgList) > 50:
 	msgList = msgList[50:]
 with open('index/index.json', 'w') as f:
 	json.dump({'msgList':msgList}, f)
-	
-for url in urlList:
-	#using the same method
-	#url is already given
-	#using the same  headers
-	res = requests.request(url=url,method=method,headers=headers)
-	
-	content = re.findall('<section.*',res.text)
-	content = os._exit(-5) if content==[] else content[0].replace('data-src','src')
-	with open('articles/'+str(msgList[-1]['id']),'w') as f:
-		f.write(content.encode('utf-8'))
-	time.sleep(30)
